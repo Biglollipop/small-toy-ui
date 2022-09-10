2bb65b25f06d8c10a595dfa0175c06344b4b0c06
chenshiqiang <csq@jovision.com>
2022-09-11 01:42:00 +0800

    git actions test2

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: chenshiqiang
+ * @Date: 2022-09-11 01:40:24
+ * @LastEditors: chenshiqiang
+ * @LastEditTime: 2022-09-11 01:40:35
+ * @version: 
+ * @Descripttion: 
+ */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -6,6 +14,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
 	plugins: [vue(), vueJsx()],
 	resolve: {
 		alias: {
