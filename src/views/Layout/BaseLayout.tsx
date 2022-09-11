109b9f915b232a154b090138a185617aa1422787
chenshiqiang <csq@jovision.com>
2022-09-11 16:57:16 +0800

    feat: 测试

---
+++ src/views/Layout/BaseLayout.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: chenshiqiang
+ * @Date: 2022-09-11 16:25:50
+ * @LastEditors: chenshiqiang
+ * @LastEditTime: 2022-09-11 16:57:04
+ * @version:
+ * @Descripttion:
+ */
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+	setup() {
+		return () => (
+			<div>
+				<h1>Login</h1>
+				<div>登录</div>
+			</div>
+		)
+	}
+})
